bf81338b9b38295a595efe8130a3349a7bf294c1
Ellis Hampton <hamptonelkav@gmail.com>
2026-04-14 16:39:54 -0700

    Add EXE stage to tick()

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -15,6 +15,8 @@ int32_t CPU::fetch(std::vector<std::string> iMem, int32_t pc){
 }
 
 void CPU::tick(){
+    // ===== IF ===== //
+
     int32_t machineCode = fetch(iMem, pc);
     std::cout << machineCode << std::endl;
     int32_t next_pc = pc + 4;
@@ -22,6 +24,28 @@ void CPU::tick(){
     std::unique_ptr<instruction> insn = disassemble(machineCode);
     control.setSignals(*insn); //needs to be implemented
 
-    
+
+    // ===== ID ===== //
+
+    int32_t readData1 = rf.read(insn->rs1);
+    int32_t readData2 = rf.read(insn->rs2);
+
+    // ===== EXE ===== //
+
+    int32_t aluOpB = control.condSignals.aluSrc ? insn->immi : readData2;
+
+    aluCtrlOp aluCtrl = control.aluCtrl(control.condSignals.ALUOP, *insn);
+    ALU::result aluRes = alu.execute(readData1, aluOpB, aluCtrl);
+
+    bool branch = control.condSignals.branch && aluRes.aluZero;
+    int32_t branchTarget = pc + insn->immi;
+
+    if (branch) next_pc = branchTarget;
+
+    // ===== MEM ===== //
+
+
+
+    // ===== WB ===== //
 
 }
