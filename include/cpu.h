ad604b207139af302c40cddc310d3b561ca610b0
Ellis Hampton <hamptonelkav@gmail.com>
2026-04-14 19:28:42 -0700

    Refactor into separate functions for each stage

--- include/cpu.h
+++ include/cpu.h
@@ -10,6 +10,7 @@
 class CPU {
     public:
         uint32_t pc;
+        uint32_t next_pc;
         uint32_t global_ticks;
 
         std::vector<std::string> iMem;
@@ -17,9 +18,14 @@ class CPU {
         RegisterFile rf;
         ControlUnit control;
         Memory memory;
+
+
         
         CPU();
         int32_t fetch(std::vector<std::string> iMem, int32_t pc);
+        std::unique_ptr<instruction> decode(int32_t machineCode);
+        ALU::result execute(instruction insn, int32_t readData1, int32_t readData2);
+        int32_t mem(ALU::result aluRes, int32_t readData2);
         void writeback(int32_t rd, int32_t val);
         void tick();
 };
--- src/cpu.cpp
+++ src/cpu.cpp
@@ -11,45 +11,35 @@ CPU::CPU(){
 }
 
 int32_t CPU::fetch(std::vector<std::string> iMem, int32_t pc){
-    return std::stol(iMem[pc/4], nullptr, 2);
-}
-
-void CPU::writeback(int32_t rd, int32_t val) {
-    rf.write(rd, val);
-    ++global_ticks;
-}
-
-void CPU::tick(){
-    // ===== IF ===== //
-
-    int32_t machineCode = fetch(iMem, pc);
+    int32_t machineCode = std::stol(iMem[pc/4], nullptr, 2);
     std::cout << machineCode << std::endl;
     int32_t next_pc = pc + 4;
 
+    return machineCode;
+}
+
+std::unique_ptr<instruction> CPU::decode(int32_t machineCode) {
     std::unique_ptr<instruction> insn = disassemble(machineCode);
     control.setSignals(*insn); //needs to be implemented
+    return insn;
+}
 
+ALU::result CPU::execute(instruction insn, int32_t readData1, int32_t readData2) {
+    int32_t aluOpB = control.condSignals.aluSrc ? insn.immi : readData2;
 
-    // ===== ID ===== //
-
-    int32_t readData1 = rf.read(insn->rs1);
-    int32_t readData2 = rf.read(insn->rs2);
-
-    // ===== EXE ===== //
-
-    int32_t aluOpB = control.condSignals.aluSrc ? insn->immi : readData2;
-
-    aluCtrlOp aluCtrl = control.aluCtrl(control.condSignals.ALUOP, *insn);
+    aluCtrlOp aluCtrl = control.aluCtrl(control.condSignals.ALUOP, insn);
     ALU::result aluRes = alu.execute(readData1, aluOpB, aluCtrl);
 
     bool branch = control.condSignals.branch && aluRes.aluZero;
-    int32_t branchTarget = pc + insn->immi;
+    int32_t branchTarget = pc + insn.immi;
 
     if (branch) next_pc = branchTarget;
     pc = next_pc;
 
-    // ===== MEM ===== //
+    return aluRes;
+}
 
+int32_t CPU::mem(ALU::result aluRes, int32_t readData2) {
     bool memRead = control.condSignals.memRead;
     bool memWrite = control.condSignals.memWrite;
 
@@ -58,16 +48,31 @@ void CPU::tick(){
     if (memWrite) {
         memory.store((int8_t)aluRes.val, readData2);
         ++global_ticks;
-        return;
+        return 0;
     }
 
     if (memRead) {
         readDataDmem = memory.load((int8_t)aluRes.val);
     }
 
-    int32_t resultData = control.condSignals.memToReg ? readDataDmem : aluRes.val;
+    return control.condSignals.memToReg ? readDataDmem : aluRes.val;
+}
 
-    // ===== WB ===== //
+void CPU::writeback(int32_t rd, int32_t val) {
+    rf.write(rd, val);
+    ++global_ticks;
+}
+
+void CPU::tick(){
+    int32_t machineCode = CPU::fetch(iMem, pc);
+
+    std::unique_ptr<instruction> insn = CPU::decode(machineCode);
+    int32_t readData1 = rf.read(insn->rs1);
+    int32_t readData2 = rf.read(insn->rs2);
+
+    ALU::result aluRes = execute(*insn, readData1, readData2);
+
+    int32_t resultData = mem(aluRes, readData2);
 
     writeback(insn->rd, resultData);
 }
